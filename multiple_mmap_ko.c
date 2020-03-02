bcb74687d578e9342869da6c1f2e8844303ca617
ilya <inbox.sis@gmail.com>
2020-03-02 18:25:16 +0300

    Updated source code

--- multiple_mmap_ko.c
+++ multiple_mmap_ko.c
@@ -1,37 +1,110 @@
 #include <linux/init.h>
 #include <linux/module.h>
 #include <linux/kernel.h>
+#include <linux/uaccess.h>
+#include <linux/dma-mapping.h>
 #include <linux/slab.h>
 #include <linux/moduleparam.h>
+#include "multiple_mmap_ko.h"
+
 
 #define DRIVER_NAME         "Multiple_mmap"
 #define DRIVER_DESCRIPTION  "Allocate dma_alloc_noncoherent buffer & check multiple mmap calls"
 
-#define InfoPrintf(arg)   _Info_Print_Macro   arg
-#define ErrorPrintf(arg)  _Error_Print_Macro  arg
+#define InfoPrintf(arg)       _Info_Print_Macro   arg
+#define ErrorPrintf(arg)      _Error_Print_Macro  arg
+#define DebugPrintf(arg)      _Debug_Print_Macro      arg
+#define DebugPrintf_Cont(arg) _Debug_Print_Cont_Macro arg
 
-#define _Info_Print_Macro(fmt, args...)   printk(KERN_INFO  DRIVER_NAME ": " fmt, ## args)
-#define _Error_Print_Macro(fmt, args...)  printk(KERN_ERR   DRIVER_NAME ": " fmt, ## args)
+#define _Info_Print_Macro(fmt, args...)       printk(KERN_INFO  DRIVER_NAME ": " fmt, ## args)
+#define _Error_Print_Macro(fmt, args...)      printk(KERN_ERR   DRIVER_NAME ": " fmt, ## args)
+#define _Debug_Print_Macro(fmt, args...)      printk(KERN_DEBUG DRIVER_NAME ": " fmt, ## args)
+#define _Debug_Print_Cont_Macro(fmt, args...) printk(KERN_CONT  fmt, ## args)
 
-#define  BUFF_LEN  (100 * 1024 * 1024)
 
-// #define  PRINT_BUFF        InfoPrintf((">> ===== pBuff =====\n"));     \
-// 			   for(i = 0; i < 20; i++)                                  \
-// 				InfoPrintf((">> pBuff[%d]:   0x%X\n", i, pBuff[i]));\
-// 			   InfoPrintf((">> pBuff[125]: 0x%X\n", pBuff[125]));       \
-// 			   InfoPrintf((">> pBuff[255]: 0x%X\n", pBuff[255]));       \
-// 			   InfoPrintf((">> =================\n"));
+#define DEVICE_FIRST  0
+#define DEVICE_COUNT  1
+#define DEVICE_NAME   "mult_mmap"
+
+#define  BUFF_SIZE (100 * 1024 * 1024)
 
 
 // Pointer to the main driver object
 DRIVER_OBJECT *pGbl_DriverObject;
 
+void mod_exit( void );
 
-
-
-
-int __init module_init(void)
+static inline void *dma_alloc_noncoherent(struct device *dev, size_t size,
+                                          dma_addr_t *dma_handle, gfp_t gfp)
 {
+    unsigned long attrs = DMA_ATTR_NON_CONSISTENT;
+    return dma_alloc_attrs(dev, size, dma_handle, gfp, attrs);
+}
+
+
+int Dispatch_mmap( struct file *filp, struct vm_area_struct *vma )
+{
+    int               rc;
+    off_t             offset;
+    // uint64_t          AddressToMap;
+    // DEVICE_EXTENSION *pdx;
+
+
+    DebugPrintf_Cont(("\n"));
+    DebugPrintf(("Received message ===> MMAP\n"));
+    DebugPrintf(("mmap: vm_start:    0x%lx\n", vma->vm_start));
+    DebugPrintf(("mmap: vm_vm_pgoff: 0x%lx\n", vma->vm_pgoff));
+    DebugPrintf(("mmap: vm_end:      0x%lx\n", vma->vm_end));
+    DebugPrintf(("mmap: size:        0x%lx\n", vma->vm_end - vma->vm_start));
+
+    rc = 0;
+
+    // Get device extension
+    //pdx = ((DEVICE_OBJECT*)(filp->private_data))->DeviceExtension;
+
+    // Get the supplied offset
+    offset = vma->vm_pgoff;
+
+
+
+
+
+
+    return rc;
+}
+
+
+long Dispatch_IoControl( struct file *filp, unsigned int cmd, unsigned long args )
+{
+  DRV_PARAMS  IoBuffer;
+  DRV_PARAMS *pIoBuffer;
+  int status;
+
+
+  // Copy the I/O Control message from user space
+  status = copy_from_user(&IoBuffer, (DRV_PARAMS*)args, sizeof(DRV_PARAMS));
+  if (status != 0) {
+    ErrorPrintf(("ERROR - Unable to copy user I/O message data\n"));
+    return (-EFAULT);
+  }
+
+  pIoBuffer = &IoBuffer;
+
+
+  DebugPrintf(("...Completed message\n"));
+
+  status = copy_to_user((DRV_PARAMS*)args, pIoBuffer, sizeof(DRV_PARAMS));
+  return status;
+}
+
+
+
+int __init mod_init( void )
+{
+  int         status;
+  dev_t       dev;
+  dma_addr_t  BusAddress;
+
 
   InfoPrintf(("Start Multiple_mmap driver\n"));
 
@@ -42,14 +115,60 @@ int __init module_init(void)
 			return (-ENOMEM);
 	}
 
+  // Fill in the appropriate dispatch handlers
+  pGbl_DriverObject->DispatchTable.owner = THIS_MODULE;
+  pGbl_DriverObject->DispatchTable.mmap  = Dispatch_mmap;
 
-    return 0;
+  // Use newer IOCTL functions
+  pGbl_DriverObject->DispatchTable.unlocked_ioctl = Dispatch_IoControl;
+  pGbl_DriverObject->DispatchTable.compat_ioctl   = Dispatch_IoControl;
+
+  // Create char device
+  status = alloc_chrdev_region(&dev, DEVICE_FIRST, DEVICE_COUNT, DRIVER_NAME);
+  pGbl_DriverObject->MajorID = MAJOR( dev );
+
+  DebugPrintf(("Registered driver (MajorID = %03d)\n", pGbl_DriverObject->MajorID));
+
+  if (pGbl_DriverObject->MajorID < 0) {
+      status = pGbl_DriverObject->MajorID;
+      ErrorPrintf(("ERROR: alloc_chrdev_region() return the error (status=%d)\n", status));
+      mod_exit();
+      return status;
+  }
+
+  cdev_init(&(pGbl_DriverObject->cdev), &(pGbl_DriverObject->DispatchTable));
+  pGbl_DriverObject->cdev.owner = THIS_MODULE;
+  pGbl_DriverObject->cdev.ops = &pGbl_DriverObject->DispatchTable;
+
+  status = cdev_add(&(pGbl_DriverObject->cdev), DEVICE_FIRST, DEVICE_COUNT);
+
+  // Allocate phys buffer
+  // pGbl_DriverObject->pKernelVa = dma_alloc_noncoherent(, BUFF_SIZE,
+  //                                               &BusAddress,
+  //                                               GFP_KERNEL | __GFP_NOWARN);
+  //
+  // if (pGbl_DriverObject->pKernelVa == NULL) {
+  //   mod_exit();
+  //   return -1;
+  // }
+
+
+  InfoPrintf(("...driver loaded\n\n"));
+  return 0;
 }
 
 
-void __exit module_exit(void)
+void mod_exit( void )
 {
-	// Release driver object
+  // Release the buffer
+  if (pGbl_DriverObject->pKernelVa) {
+    // dma_free_coherent(pdx, BUFF_SIZE, pGbl_DriverObject->pKernelVa,
+    //                   (dma_addr_t)pMemObject->BusPhysical);
+  }
+
+  cdev_del(&(pGbl_DriverObject->cdev));
+
+  // Release driver object
 	kfree( pGbl_DriverObject );
 	pGbl_DriverObject = NULL;
 
@@ -59,5 +178,5 @@ void __exit module_exit(void)
 
 MODULE_LICENSE("GPL");
 MODULE_DESCRIPTION(DRIVER_DESCRIPTION);
-module_init(module_init);
-module_exit(module_exit);
+module_init(mod_init);
+module_exit(mod_exit);
--- multiple_mmap_ko.h
+++ multiple_mmap_ko.h
@@ -1,11 +1,40 @@
+#include <linux/fs.h>
+#include <linux/cdev.h>
+
+typedef uint8_t BOOLEAN;
+
+// Information about contiguous, page-locked buffers
+typedef struct _PHYS_MEM_OBJECT
+{
+    struct list_head  ListEntry;
+    void             *pOwner;
+    uint8_t          *pKernelVa;
+    uint64_t          CpuPhysical;            // CPU Physical Address
+    uint64_t          BusPhysical;            // Bus Physical Address
+    uint32_t          Size;                   // Buffer size
+} PHYS_MEM_OBJECT;
+
+
+// Used to pass IOCTL arguments down to the driver
+typedef struct _DRV_PARAMS
+{
+    int     ReturnCode;      // API status code
+    // PLX_DEVICE_KEY Key;             // Device key information
+    uint64_t       value[3];        // Generic storage for parameters
+    uint64_t       ExData[5];
+} DRV_PARAMS;
+
+
 // Main driver object
 typedef struct _DRIVER_OBJECT
 {
     struct _DEVICE_OBJECT  *DeviceObject;     // Pointer to first device in list
     int                     MajorID;          // The OS-assigned driver Major ID
     uint8_t                 DeviceCount;      // Number of devices in list
-    //PLX_PHYS_MEM_OBJECT     CommonBuffer;     // Contiguous memory to be shared by all processes
+    PHYS_MEM_OBJECT         DmaBuffer;        // Contiguous memory to be shared by all processes
     struct file_operations  DispatchTable;    // Driver dispatch table
+
+    uint8_t                *pKernelVa;
 
     struct cdev             cdev;             // Char device structure
     struct class           *devclass;
